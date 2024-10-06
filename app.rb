58cfd963e0a60c8ee6304393badf593aeff377d4
C0de Bebop <rlatchman3624@gmail.com>
2024-10-06 05:26:20 -0700

    Initial commit

---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'securerandom'
+
+class HackerboardApp < Sinatra::Base
+
+  get '/' do
+    erb :rankings
+  end
+
+  get '/rankings' do
+    erb :rankings
+  end
+
+  get '/join' do
+    erb :join
+  end
+
+  post '/join' do
+    @country = params[:country]
+    @handle = params[:handle]
+    @language = params[:'programming-language']
+    @hacker_id = SecureRandom.hex(12)
+    if @handle.empty? || @country.empty? || @language.empty?
+      erb :join
+    else
+      redirect '/avatar'
+    end
+  end
+
+  get '/avatar' do
+    erb :avatar
+  end
+
+  get '/challenges/:id' do
+    erb :challenges
+  end
+
+ get '/challenge/:id/solved' do
+   erb :solved
+ end
+
+ get '/challenge/:id' do
+   erb :challenge
+ end
+
+
+
+
+ run!
+end
